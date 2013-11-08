2bdf3400c1e1eea33cbb4bec4cc29d5515cb2d2f
Erik Cunningham <djtrinary@gmail.com>
2013-11-08 16:23:44 -0700

    changeable breakpoint

--- connector.js
+++ connector.js
@@ -18,28 +18,33 @@ function alongY(p1, p2, r) {
 }
 
 function svg_connector_linear_step(d) {
+    var breakpoint = 0.3;
+    if( d[1][1] > d[0][1]) {
+      breakpoint = 0.6;
+    }
     return "M " + d[0] + 
-      " H " + alongX(d[0], d[1], 0.3) + 
+      " H " + alongX(d[0], d[1], breakpoint) + 
       " V"  + alongY(d[0], d[1], 1) + 
       " H"  + alongX(d[0], d[1], 1) + 
       " L " + d[2] +  // Right side straight line
-      " H " + (alongX(d[2], d[3], 0.6)) + 
+      " H " + (alongX(d[2], d[3], breakpoint)) + 
       " V"  + alongY(d[3], d[2], 0) + 
       " L " + d[3] + " Z";
 }
 
 function svg_connector() {
   var connectTypes = { "linear-step": svg_connector_linear_step } ;
-  var connectType = "linear-step";
+  var connectFn = svg_connector_linear_step;
 
 
   function connector(d) {
-    return connectTypes[connectType](d);
+    return connectFn(d);
   }
 
   connector.type = function(t) {
     if (! arguments.length) { return connectType; }
-    connectType = t;
+    if (typeof(t) == "function") { connectFn = t; }
+    else { connectFn = connectTypes[t]; }
     return connector;
   };
   return connector;
