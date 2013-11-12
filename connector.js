aed403bd8a95a4edc825f4022711f924a94059ea
Erik Cunningham <djtrinary@gmail.com>
2013-11-12 12:49:23 -0700

    wrap everything in an IIFE.

--- connector.js
+++ connector.js
@@ -1,77 +1,77 @@
-function alongXY(p1,p2, r) { 
-  return [alongX(p1, p2, r), alongY(p1,p2,r)];
-}
-
-function alongX(p1, p2, r) {
-  var dx, extents, value;
-  dx = ((p2[0] - p1[0]) * r);
-  value = p1[0] + dx;
-  return value;
-}
-
-function alongY(p1, p2, r) {
-  var dy, extents, value;
-  dy = ((p2[1] - p1[1]) * r);
-  value = p1[1] + dy;
-  return value;
-}
-
-function svg_connector_linear(d) {
-  var first = d[0], second = d[1];
-  return "M " + first[0] + " L " + second[0] +
-         " L " + second[1] + " L " + first[1] + " Z";
-}
-
-function svg_connector_linear_step(d) {
-  var first = d[0], second = d[1];
-  var breakpoint = 0.3;
-  if( second[1][1] > first[0][1]) {
-    // switch break to prevent overlap
-    breakpoint = 0.6;
-  }
-  return "M " + first[0] + 
-    " H " + alongX(first[0], second[0], breakpoint) + 
-    " V"  + alongY(first[0], second[0], 1) + 
-    " H"  + alongX(first[0], second[0], 1) + 
-    " L " + second[1] +  // second side straight line
-    " H " + (alongX(second[1], first[1], breakpoint)) + 
-    " V"  + alongY(second[1], first[1], 1) + 
-    " L " + first[1] + " Z";
-}
-
-function svg_connector() {
-  var connectTypes = { "linear-step": svg_connector_linear_step,
-                       "linear": svg_connector_linear } ;
-  var connectFn = svg_connector_linear_step;
-  var xFn = function(point) { return point[0]; },
-      yFn = function(point) { return point[1]; };
-
-  function connector(d) {
-    var points = [[xFn(d[0]), yFn(d[0])], [xFn(d[1]),yFn(d[1])]];
-    return connectFn(points);
+(function() { 
+  function alongXY(p1,p2, r) { 
+    return [alongX(p1, p2, r), alongY(p1, p2, r)];
   }
 
-  connector.type = function(t) {
-    if (! arguments.length) { return connectType; }
-    if (typeof(t) == "function") { connectFn = t }
-    else { connectFn = connectTypes[t]}
-    return connector;
-  };
+  function alongX(p1, p2, r) {
+    var dx, extents;
+    dx = ((p2[0] - p1[0]) * r);
+    return p1[0] + dx;
+  }
 
-  connector.x = function(f) {
-    if (!arguments.length) { return xFn; }
-    xFn = f;
-    return connector;
-  };
+  function alongY(p1, p2, r) {
+    var dy, extents;
+    dy = ((p2[1] - p1[1]) * r);
+    return p1[1] + dy;
+  }
 
-  connector.y = function(f) {
-    if (!arguments.length) { return yFn; }
-    yFn = f;
-    return connector;
-  };
-  return connector;
-}
+  function svg_connector_linear(d) {
+    var first = d[0], second = d[1];
+    return "M " + first[0] + " L " + second[0] +
+          " L " + second[1] + " L " + first[1] + " Z";
+  }
 
-svgConnector = function() {
-  return svg_connector();
-};
+  function svg_connector_linear_step(d) {
+    var first = d[0], second = d[1];
+    var breakpoint = 0.3;
+    if( second[1][1] > first[0][1]) {
+      // switch break to prevent overlap
+      breakpoint = 0.6;
+    }
+    return "M " + first[0] + 
+      " H " + alongX(first[0], second[0], breakpoint) + 
+      " V"  + alongY(first[0], second[0], 1) + 
+      " H"  + alongX(first[0], second[0], 1) + 
+      " L " + second[1] +  // second side straight line
+      " H " + (alongX(second[1], first[1], breakpoint)) + 
+      " V"  + alongY(second[1], first[1], 1) + 
+      " L " + first[1] + " Z";
+  }
+
+  function svg_connector() {
+    var connectTypes = { "linear-step": svg_connector_linear_step,
+                        "linear": svg_connector_linear } ;
+    var connectFn = svg_connector_linear_step;
+    var xFn = function(point) { return point[0]; },
+        yFn = function(point) { return point[1]; };
+
+    function connector(d) {
+      var points = [[xFn(d[0]), yFn(d[0])], [xFn(d[1]),yFn(d[1])]];
+      return connectFn(points);
+    }
+
+    connector.type = function(t) {
+      if (! arguments.length) { return connectType; }
+      if (typeof(t) == "function") { connectFn = t; }
+      else { connectFn = connectTypes[t]; }
+      return connector;
+    };
+
+    connector.x = function(f) {
+      if (!arguments.length) { return xFn; }
+      xFn = f;
+      return connector;
+    };
+
+    connector.y = function(f) {
+      if (!arguments.length) { return yFn; }
+      yFn = f;
+      return connector;
+    };
+    return connector;
+  }
+
+  svgConnector = function() {
+    return svg_connector();
+  };
+})();
